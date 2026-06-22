684f7f1699058c77a443fc7328ed46b1b38c401e
Leonardo Miranda Gomes <l.mirandagomes.inf25@stud.bbbaden.ch>
2026-06-22 18:35:36 +0200

    set all possible variables in js to const's

--- src/script.js
+++ src/script.js
@@ -2,16 +2,15 @@ async function populateSidebar() {
     const sections = document.querySelectorAll("main section");
     const sidebarMenu = document.querySelector(".sidebar menu");
 
-    for (let i in sections) {
-        console.log(sections[i].querySelector("h1, h2, h3, h4, h5, h6"))
-        let firstSectionHeading = sections[i].querySelector("h1, h2, h3, h4, h5, h6")
+    for (const i in sections) {
+        const firstSectionHeading = sections[i].querySelector("h1, h2, h3, h4, h5, h6")
 
-        let aItem = document.createElement("a")
+        const aItem = document.createElement("a")
         aItem.classList.add("button")
         aItem.href = "#" + sections[i].id
         aItem.textContent = firstSectionHeading.textContent
 
-        let listItem = document.createElement("li")
+        const listItem = document.createElement("li")
         listItem.appendChild(aItem)
 
         sidebarMenu.appendChild(listItem)
